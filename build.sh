ca974058c6533f57440fb34ec2e576285be18790
Tyler Johnson <mail@tyler-johnson.ca>
2017-10-03 14:09:15 -0400

    Prep for builds

---
+++ build.sh
@@ -0,0 +1,5 @@
+# remove node_modules
+# sudo npm install -g asar
+# npm install production
+# create exe / sh archive which extracts an ASAR to rvplayer/modules/[version]
+# on CCI, update remote manifest version# and rollout%
