c2448bcfcb75644013b92f16fdfe9c6c5d692809
Tyler Johnson <mail@tyler-johnson.ca>
2017-10-05 19:53:24 -0400

    Fix unused variable lint error

--- player/installer.js
+++ player/installer.js
@@ -1,6 +1,6 @@
 module.exports = {
   showFailedProxy(host) {
-    log.debug("restarting installer to show failed host");
+    log.debug(`restarting installer to show failed ${host}`);
   },
   showInvalidDisplayId() {
     log.debug("restarting installer to show invalid display id");
