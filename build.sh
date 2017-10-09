0714db39d11c41761da4f0c414f4b665e17979e1
Tyler Johnson <mail@tyler-johnson.ca>
2017-10-09 08:55:09 -0400

    Set up CircleCI

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 mkdir -p src/main/viewer/localviewer && mkdir -p widgets
 git clone git@github.com:Rise-Vision/private-keys.git
-gcloud auth activate-service-account 452091732215@developer.gserviceaccount.com --key-file ./private-keys/storag-server/rva-media-library-ce0d2bd78b54.json
+gcloud auth activate-service-account 452091732215@developer.gserviceaccount.com --key-file ./private-keys/storage-server/rva-media-library-ce0d2bd78b54.json
 gsutil -m rsync -d -r gs://install-versions.risevision.com/localviewer src/main/viewer/localviewer
 gsutil -m rsync -d -r gs://install-versions.risevision.com/widgets widgets
+rm -rf src/main/viewer/localviewer/widgets
 mv widgets src/main/viewer/localviewer
 rm -rf node_modules
 sudo npm install -g asar
@@ -10,12 +11,14 @@ npm install --production
 cp -r node_modules src/main
 rm -rf build
 mkdir -p build
+echo '{"useElectron": true}' >build/package.json
 asar p src/main build/player.asar
 makeself --notemp build player.sh "Rise Player"
 mv player.sh build
-cd build && 7z a -mx4 player.7z player.asar
+cd build && 7z a -mx4 player.7z player.asar package.json
 cat ../win-sfx/7zS.sfx ../win-sfx/sfx_config.txt player.7z >player.exe
 rm player.7z
 rm player.asar
 cd ..
 rm -rf src/main/node_modules
+rm build/package.json
--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,31 @@
-echo $(date +%Y.%m.%d.%H.%M) >version
-echo "deploying $(cat /tmp/shared-workspace/version-string/version)"
+#!/bin/bash
+MODULENAME="player"
+VERSION=$(date +%Y.%m.%d.%H.%M)
+echo "staging $VERSION"
 
-# gsutil rm -r gs://install-versions.risevision.com/master/ || true
-# gsutil -m cp -p gs://install-versions.risevision.com/staging/$(cat /tmp/shared-workspace/version-string/version)/* gs://
-# install-versions.risevision.com/master
-# gsutil cp gs://install-versions.risevision.com/electron*.json .
-# for f in electron*.json; do sed -i "s/\"InstallerElectronVersion.*/\"InstallerElectronVersion\":\"$(cat /tmp/shared-work
-# space/version-string/version)\",/" $f;done
-# for f in electron*.json; do sed -i 's/"LatestRolloutPercent.*/"LatestRolloutPercent":2,/' $f;done
-# gsutil cp electron*.json gs://install-versions.risevision.com/master/
-# gsutil setmeta -h "Cache-Control:private, max-age=0" gs://install-versions.risevision.com/master/*.{sh,exe,json}
-# gsutil setmeta -h "Content-Disposition:attachment" gs://install-versions.risevision.com/master/*.sh
-# gsutil acl ch -u AllUsers:R gs://install-versions.risevision.com/master/*.{sh,exe,json}
-# gsutil cp -p gs://install-versions.risevision.com/*.{exe,sh,json} gs://install-versions.risevision.com/backups/$(cat /tm
-# p/shared-workspace/version-string/version)/
-# gsutil -m cp -p gs://install-versions.risevision.com/master/* gs://install-versions.risevision.com/
-# gsutil -m cp -p gs://install-versions.risevision.com/master/* gs://install-versions.risevision.com/releases/$(cat /tmp/s
-# hared-workspace/version-string/version)/
-# echo -n "RisePlayerElectron $(cat /tmp/shared-workspace/version-string/version)" > latest-version
-# gsutil cp latest-version gs://install-versions.risevision.com
-# gsutil setmeta -h "Cache-Control:private, max-age=0" gs://install-versions.risevision.com/latest-version
-# gsutil setmeta -h "Content-Type:text/plain" gs://install-versions.risevision.com/latest-version
-# gsutil acl ch -u AllUsers:R gs://install-versions.risevision.com/latest-version
+gsutil cp build/* gs://install-versions.risevision.com/staging/$MODULENAME/$VERSION/
+gsutil setmeta -h "Cache-Control:private, max-age=0" gs://install-versions.risevision.com/staging/$MODULENAME/$VERSION/*
+gsutil acl ch -u AllUsers:R gs://install-versions.risevision.com/staging/$MODULENAME/$VERSION/*
+
+if [ $CIRCLE_BRANCH == "master" ]; then
+  echo "deploying to master"
+  # gsutil -m cp -p gs://install-versions.risevision.com/staging/$MODULENAME/$VERSION/* gs://install-versions.risevision.com/master
+  # gsutil cp gs://install-versions.risevision.com/electron*.json .
+  # for f in electron*.json; do sed -i "s/\"InstallerElectronVersion.*/\"InstallerElectronVersion\":\"$(cat /tmp/shared-work
+  # space/version-string/version)\",/" $f;done
+  # for f in electron*.json; do sed -i 's/"LatestRolloutPercent.*/"LatestRolloutPercent":2,/' $f;done
+  # gsutil cp electron*.json gs://install-versions.risevision.com/master/
+  # gsutil setmeta -h "Cache-Control:private, max-age=0" gs://install-versions.risevision.com/master/*.{sh,exe,json}
+  # gsutil setmeta -h "Content-Disposition:attachment" gs://install-versions.risevision.com/master/*.sh
+  # gsutil acl ch -u AllUsers:R gs://install-versions.risevision.com/master/*.{sh,exe,json}
+  # gsutil cp -p gs://install-versions.risevision.com/*.{exe,sh,json} gs://install-versions.risevision.com/backups/$(cat /tm
+  # p/shared-workspace/version-string/version)/
+  # gsutil -m cp -p gs://install-versions.risevision.com/master/* gs://install-versions.risevision.com/
+  # gsutil -m cp -p gs://install-versions.risevision.com/master/* gs://install-versions.risevision.com/releases/$(cat /tmp/s
+  # hared-workspace/version-string/version)/
+  # echo -n "RisePlayerElectron $(cat /tmp/shared-workspace/version-string/version)" > latest-version
+  # gsutil cp latest-version gs://install-versions.risevision.com
+  # gsutil setmeta -h "Cache-Control:private, max-age=0" gs://install-versions.risevision.com/latest-version
+  # gsutil setmeta -h "Content-Type:text/plain" gs://install-versions.risevision.com/latest-version
+  # gsutil acl ch -u AllUsers:R gs://install-versions.risevision.com/latest-version
+fi
