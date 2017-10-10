fdf9aab313ca2155ba36cdf7b42142391e49dbb6
Tyler Johnson <mail@tyler-johnson.ca>
2017-10-10 11:41:50 -0400

    Update build

--- build.sh
+++ build.sh
@@ -1,18 +1,20 @@
-mkdir -p src/main/viewer/localviewer && mkdir -p widgets
+rm -rf build-temp
+cp -r src/main build-temp
+mkdir -p build-temp/main/viewer/localviewer && mkdir -p build-temp/widgets
 git clone git@github.com:Rise-Vision/private-keys.git
 gcloud auth activate-service-account 452091732215@developer.gserviceaccount.com --key-file ./private-keys/storage-server/rva-media-library-ce0d2bd78b54.json
-gsutil -m rsync -d -r gs://install-versions.risevision.com/localviewer src/main/viewer/localviewer
-gsutil -m rsync -d -r gs://install-versions.risevision.com/widgets widgets
-rm -rf src/main/viewer/localviewer/widgets
-mv widgets src/main/viewer/localviewer
+gsutil -m rsync -d -r gs://install-versions.risevision.com/localviewer build-temp/main/viewer/localviewer
+gsutil -m rsync -d -r gs://install-versions.risevision.com/widgets build-temp/widgets
+rm -rf build-temp/main/viewer/localviewer/widgets
+mv build-temp/widgets build-temp/main/viewer/localviewer
 rm -rf node_modules
 sudo npm install -g asar
 npm install --production
-cp -r node_modules src/main
+cp -r node_modules build-temp/main
 rm -rf build
 mkdir -p build
 echo '{"useElectron": true}' >build/package.json
-asar p src/main build/player.asar
+asar p build-temp/main build/player.asar
 makeself --notemp build player.sh "Rise Player"
 mv player.sh build
 cd build && 7z a -mx4 player.7z player.asar package.json
@@ -20,5 +22,5 @@ cat ../win-sfx/7zS.sfx ../win-sfx/sfx_config.txt player.7z >player.exe
 rm player.7z
 rm player.asar
 cd ..
-rm -rf src/main/node_modules
+rm -rf build-temp
 rm build/package.json
