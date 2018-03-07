bfd4e369e238da5833596d81a098807114077439
santiagonoguez <santiago@nul-unu.com>
2018-03-07 11:22:44 -0600

    add restart functions
    still not calling them from flow

--- src/main/player/offline-restart-check.js
+++ src/main/player/offline-restart-check.js
@@ -1,4 +1,11 @@
+const subscriptionCheck = require("./offline-subscription-check");
+const restart = require("./restart");
+
+const WAIT_FOR_VIEWER_IN_MILLIS = 60 * 1000;
+
 let count = -1;
+let timerId = null;
+let viewerHasStarted = false;
 
 function getCount(args = process.argv.slice(1)) {
   if (count === -1) {
@@ -15,13 +22,47 @@ function shouldBeConsideredOffline(args = process.argv.slice(1)) {
   return getCount(args) >= 3;
 }
 
+function startRestartTimeoutIfRpp(schedule = setTimeout) {
+  resetTimer();
+
+  if( shouldBeConsideredOffline() ) {
+    // if already in offline, no more restarts
+    return Promise.resolve();
+  }
+
+  return subscriptionCheck.isSubscribed().then(subscribed => {
+    if (subscribed) {
+      timerId = schedule(() => {
+        viewerHasStarted || restart.restart([`--offline-restart-count=${count + 1}`]);
+      }, WAIT_FOR_VIEWER_IN_MILLIS);
+    }
+  });
+}
+
+function markViewerAsStarted() {
+  viewerHasStarted = true;
+}
+
+function resetTimer() {
+  if (timerId) {
+    clearTimeout(timerId);
+  }
+
+  timerId = null;
+}
+
 // For test purposes only.
 function reset() {
+  resetTimer();
+
   count = -1;
+  viewerHasStarted = false;
 }
 
 module.exports = {
   getCount,
   shouldBeConsideredOffline,
+  startRestartTimeoutIfRpp,
+  markViewerAsStarted,
   reset
 };
--- src/test/unit/offline-restart-check.js
+++ src/test/unit/offline-restart-check.js
@@ -1,9 +1,20 @@
 const assert = require("assert");
+const simple = require("simple-mock");
+
 const check = require("../../main/player/offline-restart-check");
+const subscriptionCheck = require("../../main/player/offline-subscription-check");
+const restart = require("../../main/player/restart");
 
 describe("Offline Restart Check / Unit", () => {
 
-  afterEach(() => check.reset());
+  beforeEach(() => {
+    simple.mock(restart, "restart").returnWith();
+  });
+
+  afterEach(() => {
+    check.reset();
+    simple.restore();
+  });
 
   it("has count of 0 if no arguments", () => {
     const count = check.getCount([]);
@@ -36,9 +47,59 @@ describe("Offline Restart Check / Unit", () => {
   });
 
   it("should be considered offline if offline-restart-count is 3", () => {
-    const offline = check.shouldBeConsideredOffline(['--unattended', '--offline-restart-count=3']);
+    const offline = check.shouldBeConsideredOffline(['--offline-restart-count=3']);
 
     assert(offline);
+  });
+
+  it("should not restart if it's already considered offline", () => {
+    check.shouldBeConsideredOffline(['--offline-restart-count=3']);
+
+    return check.startRestartTimeoutIfRpp(action => action()).then(() => {
+      assert(!restart.restart.called);
+    });
+  });
+
+  it("should not restart if it's not Rise Player Professional", () => {
+    simple.mock(subscriptionCheck, "isSubscribed").resolveWith(false);
+    check.shouldBeConsideredOffline([]);
+
+    return check.startRestartTimeoutIfRpp(action => action()).then(() => {
+      assert(!restart.restart.called);
+    });
+  });
+
+  it("should restart if its Rise Player Professional and count is 0 and viewer has not started", () => {
+    simple.mock(subscriptionCheck, "isSubscribed").resolveWith(true);
+    check.shouldBeConsideredOffline([]);
+
+    return check.startRestartTimeoutIfRpp(action => action()).then(() => {
+      assert(restart.restart.called);
+      assert.deepEqual(restart.restart.lastCall.args[0], ["--offline-restart-count=1"]);
+    });
+  });
+
+  it("should not restart if its Rise Player Professional and count is 0 and viewer has started", () => {
+    simple.mock(subscriptionCheck, "isSubscribed").resolveWith(true);
+    check.shouldBeConsideredOffline([]);
+
+    return check.startRestartTimeoutIfRpp(action => {
+      check.markViewerAsStarted();
+      action();
+    })
+    .then(() => {
+      assert(!restart.restart.called);
+    });
+  });
+
+  it("should restart if its Rise Player Professional and count is 2 and viewer has not started", () => {
+    simple.mock(subscriptionCheck, "isSubscribed").resolveWith(true);
+    check.shouldBeConsideredOffline(['--offline-restart-count=2']);
+
+    return check.startRestartTimeoutIfRpp(action => action()).then(() => {
+      assert(restart.restart.called);
+      assert.deepEqual(restart.restart.lastCall.args[0], ["--offline-restart-count=3"]);
+    });
   });
 
 });
