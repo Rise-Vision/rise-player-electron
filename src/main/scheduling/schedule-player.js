db9c3b88f5a59f5ce8e8cf8e0b0e427f3377f495
Tyler Johnson <mail@tyler-johnson.ca>
2018-12-05 18:09:49 -0500

    Play fallback url about:blank and log on bad data

--- src/main/scheduling/schedule-player.js
+++ src/main/scheduling/schedule-player.js
@@ -1,18 +1,15 @@
 const scheduleParser = require("./schedule-parser");
+const {inspect} = require("util");
 
 let playUrlHandler = ()=>{};
 
 module.exports = {
-  start(data = scheduleParser.getContent()) {
-    // Temporarily play first url and return
-    const noURL = "about:blank";
+  start(data = scheduleParser.getContent(), fallbackUrl = "about:blank") {
+    if (!validateData(data)) {
+      log.external("invalid schedule data", inspect(data));
 
-    if (!data) {return noURL;}
-    if (!data.content) {return noURL;}
-    if (!data.content.schedule) {return noURL;}
-    if (!data.content.schedule.items) {return noURL;}
-    if (!data.content.schedule.items.length) {return noURL;}
-    if (typeof data.content.schedule.items[0] !== "object") {return noURL;}
+      return playUrl(fallbackUrl);
+    }
 
     playUrl(data.content.schedule.items[0].objectReference);
   },
@@ -20,3 +17,14 @@ module.exports = {
 };
 
 function playUrl(url) {playUrlHandler(url);}
+
+function validateData(data) {
+  if (!data) {return false;}
+  if (!data.content) {return false;}
+  if (!data.content.schedule) {return false;}
+  if (!data.content.schedule.items) {return false;}
+  if (!data.content.schedule.items.length) {return false;}
+  if (typeof data.content.schedule.items[0] !== "object") {return false;}
+
+  return true;
+}
--- src/test/unit/scheduling/schedule-player.js
+++ src/test/unit/scheduling/schedule-player.js
@@ -1,7 +1,39 @@
 const assert = require("assert");
+const simple = require("simple-mock");
+const schedulePlayer = require("../../../main/scheduling/schedule-player");
 
-describe("Schedule Player", ()=>{
-  it("exists", ()=>{
-    assert.ok(true);
+describe.only("Schedule Player", ()=>{
+  const played = [];
+  const fallbackUrl = "about:blank";
+
+  beforeEach(()=>{
+    simple.mock(global.log, "external").returnWith();
+  });
+
+  afterEach(()=>{
+    simple.restore();
+    played.length = 0;
+  });
+
+  describe("Schedule Data Validation", ()=>{
+    const testData = [
+      null,
+      {},
+      {content: {}},
+      {content: {schedule: {}}},
+      {content: {schedule: {items: []}}},
+      {content: {schedule: {items: ['invalid']}}},
+    ];
+
+    testData.forEach(test=>{
+      it(`plays fallback url and logs external for ${JSON.stringify(test)}`, ()=>{
+        schedulePlayer.setPlayUrlHandler(url=>played.push(url));
+
+        schedulePlayer.start(test, fallbackUrl);
+
+        assert.equal(global.log.external.lastCall.args[0], "invalid schedule data");
+        assert(played[0], fallbackUrl);
+      });
+    });
   });
 });
