c2ea777921f831b839883e8530b07d1f45e5b917
André Luiz Cardoso <andrecardoso@gmail.com>
2018-12-06 09:45:38 -0200

    Moving backend calls out of injected player configuration function

--- src/main/viewer/preload/player-configuration.js
+++ src/main/viewer/preload/player-configuration.js
@@ -3,20 +3,19 @@ const commonConfig = require("common-display-module");
 const gcs = require("../../player/gcs");
 
 const displaySettings = commonConfig.getDisplaySettingsSync();
+const playerType = commonConfig.isBetaLauncher() ? "beta" : "stable";
+const playerVersion = commonConfig.getLatestVersionInManifest();
+const os = platform.getOSDescription();
+
+const displayId = displaySettings.displayid;
+const data = gcs.getCachedFileContentsSync(`risevision-display-notifications/${displayId}/content.json`);
+
+let companyId = "";
+if (data && data.content && data.content.schedule) {
+  companyId = data.content.schedule.companyId;
+}
 
 window.getRisePlayerConfiguration = function() {
-  const playerType = commonConfig.isBetaLauncher() ? "beta" : "stable";
-  const playerVersion = commonConfig.getLatestVersionInManifest();
-  const os = platform.getOSDescription();
-
-  const displayId = displaySettings.displayid;
-  const data = gcs.getCachedFileContentsSync(`risevision-display-notifications/${displayId}/content.json`);
-
-  let companyId = "";
-  if (data && data.content && data.content.schedule) {
-    companyId = data.content.schedule.companyId;
-  }
-
   return {
     playerInfo: {
       displayId,
