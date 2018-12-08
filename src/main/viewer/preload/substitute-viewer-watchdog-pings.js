8cb235efa58c0f6d18d3ccd4518a96092d949227
Tyler Johnson <mail@tyler-johnson.ca>
2018-12-07 21:53:26 -0500

    Send a substitute call without an initial delay
    If the page is cycling every ten seconds, there will be no substitute
    ping sent if there's an initial delay.

--- src/main/viewer/preload/substitute-viewer-watchdog-pings.js
+++ src/main/viewer/preload/substitute-viewer-watchdog-pings.js
@@ -1,17 +1,18 @@
 (function() {
   const ipc = require("electron").ipcRenderer;
 
-  let intervalId;
-
   ipc.on("begin-substituting-viewer-pings-to-watchdog", ()=>{
-    const withinThreeMinutesMillis = 165000;
-
-    intervalId = setInterval(()=>{
-      ipc.send("viewer-message", {
-        message: "ping",
-        from: "viewer"
-      });
-    }, withinThreeMinutesMillis);
+    sendSubstitutePings();
   });
 
+  function sendSubstitutePings() {
+    const withinThreeMinutesMillis = 165000;
+
+    ipc.send("viewer-message", {
+      message: "ping",
+      from: "viewer"
+    });
+
+    setTimeout(sendSubstitutePings, withinThreeMinutesMillis);
+  }
 }());
