a5dfea2987be4f17b1957691f79ae9cd430febd3
André Luiz Cardoso <andrecardoso@gmail.com>
2019-03-06 14:39:49 -0300

    Check external URL items on white screen event

--- src/main/player/white-screen-analyser.js
+++ src/main/player/white-screen-analyser.js
@@ -1,12 +1,32 @@
 const messaging = require("./messaging");
+const {net} = require("electron");
 
 let contentData;
+
+function checkURL(url) {
+  const request = net.request(url);
+  request.on('response', response => {
+    console.log(`STATUS: ${response.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
+    response.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+    response.on('end', () => {
+      console.log('No more data in response.');
+    });
+  });
+  request.end();
+}
 
 function checkExternalContent() {
   if (!contentData) {
     return;
   }
 
+  const riseVisionURLs = /(http(s)?:\/\/)?storage\.googleapis\.com\/risemedialibrary.+|(http(s)?:\/\/)?widgets\.risevision\.com\/.+/;
+  const scheduleItems = contentData.content.schedule.items;
+  const externalURLs = scheduleItems.filter(item => item.type === "url" && !riseVisionURLs.test(item.objectReference));
+  externalURLs.forEach(url => checkURL(url));
 }
 
 module.exports = {
