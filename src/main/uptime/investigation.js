79251f21f2bee9bcbadb1fce34db5caf36ffbf58
Tyler Johnson <mail@tyler-johnson.ca>
2019-03-18 15:39:52 -0400

    Update aws url

--- src/main/uptime/investigation.js
+++ src/main/uptime/investigation.js
@@ -6,7 +6,8 @@ const siteList = [
   "https://viewer.risevision.com",
   "https://storage-dot-rvaserver2.appspot.com",
   "https://store.risevision.com",
-  "https://aws.amazon.com/s3/",
+  "http://s3.amazonaws.com/widget-video-rv/1.1.0/dist/widget.html",
+  "https://s3.amazonaws.com/widget-video-rv/1.1.0/dist/widget.html",
   "https://storage.googleapis.com/install-versions.risevision.com/display-modules-manifest.json",
   "https://www.googleapis.com/storage/v1/b/install-versions.risevision.com/o/installer-win-64.exe?fields=kind"
 ];
