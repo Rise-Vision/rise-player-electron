49dc726fc114090dfb65685a1b53f378a933e350
Alex Deaconu <alex.deaconu@gmail.com>
2019-06-20 17:33:53 -0400

    Update logic, logging

--- src/main/uptime/content-uptime.js
+++ src/main/uptime/content-uptime.js
@@ -6,18 +6,33 @@ const templateUptimeLogger = require("../loggers/template-uptime-logger");
 const uptimeInterval = 10000;
 const responseTimeout = 3000;
 let responseTimeoutId = null;
+let playingItem = null;
 let expectedTemplate = null;
 
 function handleUptimeResponse(response) {
   log.file("uptime - result", JSON.stringify(response));
+
+  if (!expectedTemplate || !isValidResponse(response)) {
+    logger.log('uptime - invalid result', JSON.stringify(response));
+    return;
+  }
+
   clearTimeout(responseTimeoutId);
+  expectedTemplate = null;
+
   templateUptimeLogger.logTemplateUptime(response.presentationId, response.templateProductCode, response.templateVersion, true, response.errorValue);
 }
 
+function isValidResponse(response) {
+  return response && response.template && response.components &&
+    response.template.presentation_id === expectedTemplate.presentationId;
+}
+
 function handleNoResponse() {
-  log.file("uptime - no response");
+  log.file("uptime - no response", JSON.stringify(expectedTemplate));
   if (expectedTemplate) {
     templateUptimeLogger.logTemplateUptime(expectedTemplate.presentationId, expectedTemplate.productCode, expectedTemplate.version, false, null);
+    expectedTemplate = null;
   }
 }
 
@@ -29,24 +44,25 @@ function init() {
 }
 
 function retrieveUptime() {
-  if (expectedTemplate && expectedTemplate.presentationType === "HTML Template" && uptime.isActive()) {
+  if (playingItem && playingItem.presentationType === "HTML Template" && uptime.isActive()) {
     commonMessaging.broadcastToLocalWS({
       from: "player-electron",
       topic: "content-uptime", 
-      forPresentationId: expectedTemplate.presentationId
+      forPresentationId: playingItem.presentationId
     });
+    expectedTemplate = playingItem;
     responseTimeoutId = setTimeout(handleNoResponse, responseTimeout);
   }
 }
 
 function handlePlayingItemFromViewer(response) {
-  log.file("playing item received from Viewer", response);
+  log.file("playing item received from Viewer", response && JSON.stringify(response));
   response && response.item && handlePlayingItem(response.item);
 }
 
 function handlePlayingItem(item) {
-  log.file("playing item received", item);
-  expectedTemplate = item;
+  log.file("playing item received", JSON.stringify(item));
+  playingItem = item;
 }
 
 module.exports = {
